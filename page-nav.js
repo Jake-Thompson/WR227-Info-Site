6f55eb3e2350722dfa6dfe4855618d281d2feb06
Jake-Thompson <72763455+Jake-Thompson@users.noreply.github.com>
2024-03-08 18:32:29 -0800

    Add files via upload

--- page-nav.js
+++ page-nav.js
@@ -1,4 +1,4 @@
-fetch('/nav.html').then((res) => {
+fetch('nav.html').then((res) => {
     res.text().then((t) => {
         let header = document.createElement('header')
         let main = document.createElement('main')
@@ -8,8 +8,8 @@ fetch('/nav.html').then((res) => {
         SubMenuBgPos()
     })
 })
-document.title = document.title.replace('%pages%', window.location.pathname.replace(/^\/|\/$/g, '').split('/').map(x => {
+document.title = document.title.replace('%pages%', window.location.pathname.replace(/^\/|\/$/g, '').split('/').slice(1).map(x => {
     return x.replaceAll('-', ' ').replace(/\w+/g, (out) => {
         return out[0].toUpperCase() + out.slice(1).toLowerCase()
     })
-}))
+}).reverse().join(' | '))
