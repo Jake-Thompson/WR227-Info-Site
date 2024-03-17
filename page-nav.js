3b6f348090f6f99a05578d6aec6d3963e407b6f0
Jake-Thompson <jake.thompson.8939@mail.linnbenton.edu>
2024-03-16 17:38:51 -0700

    updated how page title crumbs works

--- page-nav.js
+++ page-nav.js
@@ -4,12 +4,18 @@ fetch('nav.html').then((res) => {
         let main = document.createElement('main')
         header.innerHTML = t
         main.append(...document.body.childNodes)
+        
         document.body.replaceChildren(header,main)
+        if (window.location.search == '?nav=no') header.hidden = true
         SubMenuBgPos()
     })
 })
-document.title = document.title.replace('%pages%', window.location.pathname.replace(/^\/|\/$/g, '').split('/').slice(1).map(x => {
+// document.title =  + ' | Informational Website | Jake Thompson'
+var title_tmpl = document.head.querySelector('meta[name="title-tmpl"]').content
+var crumbs = window.location.pathname.replace(/^\/|\/$/g, '').split('/').slice(1).map(x => {
     return x.replaceAll('-', ' ').replace(/\w+/g, (out) => {
         return out[0].toUpperCase() + out.slice(1).toLowerCase()
     })
-}).reverse().join(' | '))
+}).reverse().join(' | ')
+
+document.title = title_tmpl.replace('%pages%', crumbs)
